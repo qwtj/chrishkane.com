386c2154018a705bf60e0371884de5000a977b21
user156 <user156@admin34s-MacBook-Air.local>
2022-05-07 17:01:11 -0500

    add resume path

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
+import { Routes, Route, Link } from "react-router-dom";
+import resume from './resume.png'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="resume" element={<Resume />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Chris H Kane
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="resume"
           rel="noopener noreferrer"
         >
-          Learn React
+          Peek Resume
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function Resume() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={resume} className="App-resume" alt="resume" />
+        <a
+          className="App-link"
+          href="/"
+          rel="noopener noreferrer"
+        >
+          Home
         </a>
       </header>
     </div>
