7c4e8482e6dde9ab572962ad3f6bce2bbeded914
user156 <user156@admin34s-MacBook-Air.local>
2022-05-12 09:38:43 -0500

    use svg for lighting effects on certs

--- src/App.js
+++ src/App.js
@@ -30,24 +30,82 @@ class Home extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
-              src={cloudPractitioner}
-              className="App-logo"
-              loading="lazy"
-              alt="cloud practitioner"
-            />
+            {/* ample rectangle */}
+            <svg className="App-logo" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+              <defs>
+                <filter id="cloudPractioner">
+                  <feImage xlinkHref={cloudPractitioner} />
+                </filter>
+                <filter id="MyFilter" filterUnits="userSpaceOnUse"
+                      x="0" y="0"
+                      width="100%" height="100%">
+                   {/* offsetBlur */}
+                  <feGaussianBlur in="SourceAlpha" stdDeviation="4" result="blur"/>
+                  <feOffset in="blur" dx="4" dy="4" result="offsetBlur"/>
+                  {/* litPaint */}
+                  <feSpecularLighting in="blur" surfaceScale="4" specularConstant=".75"
+                                      specularExponent="100" lightingColor="#999999"
+                                      result="specOut">
+                    <fePointLight x="-5000" y="-10000" z="20000"/>
+                  </feSpecularLighting>
+                  <feComposite in="specOut" in2="SourceAlpha" operator="in" result="specOut"/>
+                  <feComposite in="SourceGraphic" in2="specOut" operator="arithmetic"
+                              k1="0" k2="1" k3="1" k4="0" result="litPaint"/>
+
+                  {/* merge offsetBlur + litPaint */}
+                  <feMerge>
+                    <feMergeNode in="offsetBlur"/>
+                    <feMergeNode in="litPaint"/>
+                  </feMerge>
+                </filter>
+              </defs>
+              {/* Graphic elements */}
+              <g filter="url(#MyFilter)">
+                  <rect x="10%" y="10%" height="80%" width="80%"
+                  style={{filter: "url(#cloudPractioner)" }} />
+              </g>
+            </svg>
           </a>
           <a
             href="https://www.credly.com/badges/3821b7d4-8993-4e28-9569-2a8dedb83b8a/public_url"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img
-              src={associateArchitect}
-              className="App-logo"
-              loading="lazy"
-              alt="associate architect"
-            />
+            {/* ample rectangle */}
+            <svg className="App-logo" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+              <defs>
+                <filter id="associateArchitect">
+                  <feImage xlinkHref={associateArchitect} />
+                </filter>
+                <filter id="MyFilter" filterUnits="userSpaceOnUse"
+                      x="0" y="0"
+                      width="100%" height="100%">
+                   {/* offsetBlur */}
+                  <feGaussianBlur in="SourceAlpha" stdDeviation="4" result="blur"/>
+                  <feOffset in="blur" dx="4" dy="4" result="offsetBlur"/>
+                  {/* litPaint */}
+                  <feSpecularLighting in="blur" surfaceScale="4" specularConstant=".75"
+                                      specularExponent="100" lightingColor="#999999"
+                                      result="specOut">
+                    <fePointLight x="-5000" y="-10000" z="20000"/>
+                  </feSpecularLighting>
+                  <feComposite in="specOut" in2="SourceAlpha" operator="in" result="specOut"/>
+                  <feComposite in="SourceGraphic" in2="specOut" operator="arithmetic"
+                              k1="0" k2="1" k3="1" k4="0" result="litPaint"/>
+
+                  {/* merge offsetBlur + litPaint */}
+                  <feMerge>
+                    <feMergeNode in="offsetBlur"/>
+                    <feMergeNode in="litPaint"/>
+                  </feMerge>
+                </filter>
+              </defs>
+              {/* Graphic elements */}
+              <g filter="url(#MyFilter)">
+                  <rect x="10%" y="10%" height="80%" width="80%"
+                  style={{filter: "url(#associateArchitect)" }} />
+              </g>
+            </svg>
           </a>
         </div>
         <p className="name">
