fd9350240fc607aa17234d1777abc346d709f8aa
Christopher Kane <91036491+wlsidlsi@users.noreply.github.com>
2025-05-10 21:53:54 -0500

    Remove unused Layout.css and related styles
    Removed the Layout.css file as it was no longer needed.
    The styles it contained have been inlined or are no longer
    required. This change streamlines the project's structure
    and removes unnecessary files.

--- src/core/Layout.jsx
+++ src/core/Layout.jsx
@@ -1,6 +1,5 @@
 import { Outlet, NavLink } from "react-router-dom";
 import Resume from "assets/resume.pdf";
-import "./Layout.css"; // Import your CSS file for styling
 
 const Layout = () => {
     return (
@@ -16,12 +15,11 @@ const Layout = () => {
           <span> | </span>
           <a href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
         </nav>
-        <hr />
         <main>
           <Outlet /> {/* Child routes will render here */}
         </main>
         <footer>
-          <p>&copy; 2023 Chrsistosfsfpher Kane</p>
+          <p>&copy; 2023 Christopher Kane</p>
         </footer>
       </>
     );
