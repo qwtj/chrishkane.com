af02a0e93ff330db8e10d50fa44db5f217edb877
Christopher Kane <91036491+wlsidlsi@users.noreply.github.com>
2025-05-10 21:52:09 -0500

    Clear features/Roles for now.

--- src/features/Roles.jsx
+++ src/features/Roles.jsx
@@ -1,15 +1,7 @@
 const Roles = () => {
     return (
-        <div>
-            <h1>Job Roles</h1>
-            <p>Here are some of the job roles I have held:</p>
-            <ul>
-                <li>Software Engineer</li>
-                <li>Data Analyst</li>
-                <li>Project Manager</li>
-                <li>DevOps Engineer</li>
-            </ul>
-        </div>
+        <>
+        </>
     );
 };
 
