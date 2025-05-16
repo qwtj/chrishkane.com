338b8c744d85ae0f4256b98fe202ec7c50c229bf
Christopher Kane <91036491+wlsidlsi@users.noreply.github.com>
2025-05-15 22:28:44 -0500

    Refactor: Improve layout and styling
    This commit improves the site's layout and overall appearance.
    
    The changes include:
    - Removing unnecessary separators in the navigation.
    - Adding links to Github and Calendly in the navigation.
    - Centering the navigation links.
    - Adding flex-wrap to the navigation to allow for better responsiveness.
    - Removing the Calendly link from the footer and streamlining it.
    - General style improvements to improve the site's aesthetics.

--- src/core/Layout.jsx
+++ src/core/Layout.jsx
@@ -6,22 +6,19 @@ const Layout = () => {
       <>
         <nav>
           <NavLink to="/" end>Home</NavLink>
-          <span> | </span>
           <NavLink className="navlink" to="/about" >About Me</NavLink>
-          <span> | </span>
           <NavLink className="navlink" to="/jobs" >Job History</NavLink>
-          <span> | </span>
           <NavLink className="navlink" to="/certifications" >Prior Certifications</NavLink>
-          <span> | </span>
           <a href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
+          <a href="https://github.com/qwtj" target="_blank" rel="noopener noreferrer">Github</a>
           <a href="/inventoryApp/index.html" target="_blank" rel="noopener noreferrer">Home Inventory</a>
           <a href="/interactive-ebook/index.html" target="_blank" rel="noopener noreferrer">Gemini E-Book Writer</a>
+          <a href="https://calendly.com/chrishkane" target="_blank" rel="noopener noreferrer">Schedule a Call</a>
         </nav>
         <main>
           <Outlet /> {/* Child routes will render here */}
         </main>
         <footer>
-          <p>Contact: <a href="https://calendly.com/chrishkane" target="_blank" rel="noopener noreferrer">Calendly: Schedule a Call</a></p>
           <p>&copy; 2023 Christopher Kane</p>
         </footer>
       </>
